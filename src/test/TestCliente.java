ebd9975f397299d2dbc060f7387f9f0ed03bd818
Marcos Guzmán <marcosguzzz@gmail.com>
2021-02-19 11:50:28 -0500

    Creación de las clases Persona, Cliente y CategoríaCliente, se utilizó herencia

---
+++ src/test/TestCliente.java
@@ -0,0 +1,37 @@
+package test;
+
+import cine.*;
+
+/**
+ *
+ * @author Marcos
+ */
+public class TestCliente {
+
+    public static void main(String args[]) {
+
+        Cliente cliente = new Cliente();
+        cliente.setNombre("Marcos");
+        cliente.setApellido("Guzmán");
+        cliente.setRuc("0850266248001");
+        cliente.setEmail("marcosguzzz@gmail.com");
+        cliente.setDireccion("Cochapamba Sur");
+        cliente.setFechaCumpleaños("24-04-2005");
+        cliente.setTelefono("0999286146");
+        cliente.setEstado(true);
+        cliente.setCategoriaCliente(categoriaCliente);
+         
+
+    }
+
+    public static CategoriaCliente obtenerCategoriaDeBDD() {
+        CategoriaCliente categoriaCliente = new CategoriaCliente();
+        categoriaCliente.setIdCategoriaCliente(0);
+        categoriaCliente.setNombre("Habitual");
+        categoriaCliente.setDescripcion("Cliente de preferencia");
+        categoriaCliente.setEstado(true);
+        
+        return categoriaCliente;
+
+    }
+}
